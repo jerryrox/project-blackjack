e5b71724dc1e2d6f7b8caebc4eedd8c8f8524e94
Jerry Kim <jerryrox@live.co.kr>
2019-08-01 23:50:22 +1200

    Fixed bug

--- src/game/rulesets/BaseRuleset.java
+++ src/game/rulesets/BaseRuleset.java
@@ -47,7 +47,7 @@ public abstract class BaseRuleset implements IGameSession {
     
     public @Override void OnStopSession()
     {
-        gameProcessor.OnStartSession();
+        gameProcessor.OnStopSession();
         drawableRuleset.OnStopSession();
         
         // Remove game processor dependency.
